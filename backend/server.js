bf7c7e4d7486c26e062e3a6d9e6f47edf97e99e8
dmitriy <dimseboms@gmail.com>
2022-05-13 15:30:05 +0200

    Endret navn på backend port for Heroku kompatibilitet

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 
 // initialiserer express og henter nødvendige variabler fra .env
 dotenv.config()
-const port = process.env.PORT || 8000
+const port = process.env.API_PORT || 8000
 const app = express()
 
 // databasetilkobling
