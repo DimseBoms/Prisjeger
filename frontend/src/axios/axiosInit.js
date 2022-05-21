fe0e3864ae0ff1fc48d407e423790fcf6bc05afe
Dmitriy Safiullin <dmisaf@outlook.com>
2022-05-21 12:58:35 +0200

    Lagt til starttjenester/startskript for server

--- frontend/src/axios/axiosInit.js
+++ frontend/src/axios/axiosInit.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:6969/api/",
+  baseURL: "http://prisjeger-app.duckdns.org:6969/api/",
   headers: {
     "Content-type": "application/json"
   }
