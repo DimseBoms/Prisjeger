2f3db3a2db8ed1f0fd08ed23b9033fa747951c87
Dmitriy Safiullin <dmisaf@outlook.com>
2022-05-23 18:18:37 +0200

    Fikset Cors error for vårt API

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,13 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: true
   }));
-  
+// Tillat alle cors forespørsler
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use('/api', prisdataruter)
 
 app.listen(port, () => console.log(`Backend server startet på port: ${port}`))
