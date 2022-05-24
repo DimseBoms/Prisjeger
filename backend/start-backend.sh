9c1ce0842170f16e4488b8247ebf3ecd019e265b
Dmitriy Safiullin <dmisaf@outlook.com>
2022-05-24 15:19:26 +0200

    Endret node versjon som benyttes for front/back

--- backend/start-backend.sh
+++ backend/start-backend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Forsøker å starte backend...
 cd /home/pi/applikasjon/prisjeger/backend
+nvm use 16.15.0
 npm run devStart
--- frontend/start-frontend.sh
+++ frontend/start-frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Forsøker å starte frontend...
 cd /home/pi/applikasjon/prisjeger/frontend
+nvm use system
 serve -s -C -n -p 3000 build
