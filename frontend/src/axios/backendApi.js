2003c8c9e241a2e32543bc8bbd75daaabcd74496
Dmitriy Safiullin <dmisaf@outlook.com>
2022-05-25 19:55:12 +0200

    Revidert Axios metoder

--- frontend/src/axios/backendApi.js
+++ frontend/src/axios/backendApi.js
@@ -22,11 +22,11 @@ class BackendApi {
       return http.get(`/vare/${vare}`)
     }
     // Metode for å vise all prisdata for en vare fra et tidspunkt
-    getVare(vare, fradato) {
+    getVareFra(vare, fradato) {
       return http.get(`/vare/${vare}/${fradato}`)
     }
     // Metode for å vise all prisdata for en vare mellom to tidspunkt
-    getVare(vare, fradato, tildato) {
+    getVareFraTil(vare, fradato, tildato) {
       return http.get(`/vare/${vare}/${fradato}/${tildato}`)
     }
     // Metode for å vise all prisdata for en butikk
@@ -34,12 +34,12 @@ class BackendApi {
       return http.get(`/butikk/${butikk}`)
     }
     // Metode for å vise all prisdata for en butikk fra et tidspunkt
-    getButikk(butikk, fradato) {
+    getButikkFra(butikk, fradato) {
       console.log('henter varedata')
       return http.get(`/butikk/${butikk}/${fradato}`)
     }
     // Metode for å vise all prisdata for en butikk mellom to tidspunkt
-    getButikk(butikk, fradato, tildato) {
+    getButikkFraTil(butikk, fradato, tildato) {
       return http.get(`/butikk/${butikk}/${fradato}/${tildato}`)
     }
     getLogStatus(lvl, status){
