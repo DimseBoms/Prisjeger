074a352b57e03b62d43247b9fd87c57418e8b6a6
Gaute <gaute_hermansen@live.no>
2022-05-28 16:28:35 +0200

    Oppdatert dashboard, og ordnet bugs med språkfunksjonalitet

--- frontend/src/variables/sampledata.js
+++ frontend/src/variables/sampledata.js
@@ -1,100 +1,35 @@
   
 export const labels = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+  "Kiwi",
+  "Meny",
+  "Coop Obs",
+  "Rema 1000",
+  "Spar",
+  "Coop Extra",
 ];
   
 //Datasets
 export const datasets = [
   {
-    label: '1',
-    data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    fill: true,
-    borderColor: "#ce13136d",
-    backgroundColor: "transparent",
-    pointBorderColor: "#ce13136d",
-    pointHoverRadius: 4,
-    pointBorderWidth: 8,
-  },
-  {
-    label: '2',
-    data: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-    fill: true,
-    borderColor: "#ce13136d",
-    backgroundColor: "transparent",
-    pointBorderColor: "#ce13136d",
-    pointHoverRadius: 4,
-    pointBorderWidth: 8,
-  },
-  {
-    label: '3',
-    labelTextColor: "#00ff11",
-    data: [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20],
-    fill: true,
-    borderColor: "#00ff11",
-    backgroundColor: "transparent",
-    pointBorderColor: "#00ff11",
-    pointHoverRadius: 4,
-    pointBorderWidth: 8,
-  },
-  {
-    label: '4',
-    data: [30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30],
-    fill: true,
-    borderColor: "#ef8157",
-    backgroundColor: "transparent",
-    pointBorderColor: "#ef8157",
-    pointHoverRadius: 4,
-    pointBorderWidth: 8,
-  },
-  {
-    label: '5',
-    data: [40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40],
-    fill: true,
-    borderColor: "#d753c6",
-    backgroundColor: "transparent",
-    pointBorderColor: "#d753c6",
-    pointHoverRadius: 4,
-    pointBorderWidth: 8,
-  },
-  {
-    label: '6',
-    data: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50],
-    fill: true,
-    borderColor: "#d753c6",
-    backgroundColor: "transparent",
-    pointBorderColor: "#d753c6",
-    pointHoverRadius: 4,
-    pointBorderWidth: 8,
-  },
+    data: [1, 1, 1, 1, 1, 1, 1],
+    borderColor: "#696969",
+    backgroundColor: [
+      "#7CFC00",  
+      "#A52A2A",  
+      "#000080",  
+      "#4169E1", 
+      "#008000",  
+      "#DAA520"],
+  }
 ];
 
 export const options = {
-    plugins: {
-      legend: { 
-        pointRadius: 1,
-        display: true, 
-        labels: {
-          font: {
-            size: 17,
-            weight: 'bold',
-          },
-        },
-        layout: {
-        padding: 20,
-      } 
+  responsive: true,
+  plugins: {legend: { display: false },
+    title: {
+      display: false,
     },
   },
-}
+};
 
   
