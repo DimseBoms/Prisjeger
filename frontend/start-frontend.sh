31326f473a44302da977c21b819d156cedff2ae6
DimseBoms <72513291+DimseBoms@users.noreply.github.com>
2022-10-10 17:54:28 +0200

    Update start-frontend.sh

--- frontend/start-frontend.sh
+++ frontend/start-frontend.sh
@@ -4,12 +4,7 @@
 # nytt dersom det slutter å kjøre ved f.eks systemkræsj.
 # Dmitriy Safiullin
 
-cd /home/pi/applikasjon/prisjeger/frontend
-
-# Henter nvm
-echo -e "\nHenter nvm...\n"
-source /home/pi/.nvm/nvm.sh
 
 echo -e "\nForsøker å starte frontend...\n"
-nvm use system
-/usr/bin/npm start
+cd "$HOME/prisjeger/frontend"
+npm start
