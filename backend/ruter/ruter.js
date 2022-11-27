7b5f056fd086d3c4484638c7a2b3b8652e848afc
DimseBoms <dmisaf@outlook.com>
2022-11-27 17:01:28 +0100

    lagt til debug i sjekkOppdatert

--- backend/ruter/ruter.js
+++ backend/ruter/ruter.js
@@ -458,6 +458,7 @@ function sjekkOppdatert(prisUtdatert, tidspunkt, brukernavn, session, handlelist
                 })
             } catch (error) {
                 console.log(`Feilmelding: ${brukernavn}: forsøker å lese en ikke-eksisterende logg`)
+                console.log(error)
             }
             // Konstruerer svar
             console.log(`${brukernavn}: prisUtdatert: ${prisUtdatert} handlelisteUtdatert: ${handlelisteUtdatert}`)
